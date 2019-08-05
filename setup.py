2d3bb7471ee34a0c8ef529884ead96dd08d1bbb6
Chris Barnes <barnesc@janelia.hhmi.org>
2019-08-05 13:57:21 -0400

    setup.py changes
    - Install heimdall package
    - Include dependencies
      - With extras
    - console scripts

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 import runpy
-from setuptools import setup
+import itertools
+from setuptools import setup, find_packages
 
 __version__ = runpy.run_path('heimdall/version.py')['__version__']
-setup(name='heimdall',
-      version=__version__,
-      author='Constantin Pape',
-      url='https://github.com/constantinpape/heimdall',
-      license='MIT',
-      scripts='scrips/view_contanier')
+
+requires = [
+    "napari",
+    "numpy",
+]
+
+extras = {
+    "hdf5": ["h5py"],
+}
+
+extras["all"] = list(itertools.chain.from_iterable(extras.values()))
+
+setup(
+    name='heimdall',
+    packages=find_packages(include='heimdall'),
+    version=__version__,
+    author='Constantin Pape',
+    install_requires=requires,
+    extras_require=extras,
+    url='https://github.com/constantinpape/heimdall',
+    license='MIT',
+    entry_points={
+        "console_scripts": ["view_container = heimdall.scripts.view_container:main"]
+    },
+)
